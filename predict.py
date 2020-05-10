edb0a84d2bcb40a91bd60e37fcaca52a8f840d81
cswanson618 <54300477+cswanson618@users.noreply.github.com>
2020-05-09 23:07:02 -0500

    Best model I could write.

---
+++ predict.py
@@ -0,0 +1,31 @@
+from joblib import load
+import pandas as pd
+
+def predict_from_csv(path_to_csv):
+
+    df = pd.read_csv(path_to_csv)
+    df = df.assign(length_in_inches = (df["Length3"]*0.39370))
+    df = df.assign(weightfactor = (df["length_in_inches"]*(df["length_in_inches"]*.58)*(df["length_in_inches"]*.58)/900))
+    X = df[["length_in_inches", "weightfactor"]].values
+    y = df[["Weight"]].values.ravel()
+    reg = load("reg.joblib")
+
+    predictions = reg.predict(X)
+
+    return predictions
+
+if __name__ == "__main__":
+    predictions = predict_from_csv("fish_holdout_demo.csv")
+    print(predictions)
+######
+
+# ### WE WRITE THIS ###
+    from sklearn.metrics import mean_squared_error, r2_score
+    ho_predictions = predict_from_csv("fish_holdout_demo.csv")
+    ho_truth = pd.read_csv("fish_holdout_demo.csv")["Weight"].values
+    ho_mse = mean_squared_error(ho_truth, ho_predictions)
+    rsqu = r2_score(ho_truth, ho_predictions)
+    print(ho_mse)
+    print(rsqu)
+# ######
+
